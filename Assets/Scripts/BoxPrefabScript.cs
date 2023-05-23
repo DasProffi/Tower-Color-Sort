a98ad087e731f77c928437400e892e529dbcdd64
DasProffi <felix.thape@gmail.com>
2023-05-23 13:42:18 +0200

    feat: add Prefabs and fix boxes being shown twice

--- Assets/Scripts/BoxPrefabScript.cs
+++ Assets/Scripts/BoxPrefabScript.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 public class BoxPrefabScript : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;  // Reference to the Image component of the box
-    private RectTransform _rectTransform;
-    
-    // Use this method to initialize the box properties
-    public void Start()
-    {
-        _rectTransform = GetComponent<RectTransform>();
-    }
 
     public void SetColor(Color boxColor)
     {
--- Assets/Scripts/TowerRenderer.cs
+++ Assets/Scripts/TowerRenderer.cs
@@ -6,31 +6,25 @@ public class TowerRenderer : MonoBehaviour
     public GameObject boxPrefab; 
     private Tower _tower= new Tower();
     private bool _isSelected = true;
-    private bool _clicked = false;
-    
-    private const float BoxWidth = 200f;   // Width of each box
-    private const float BoxHeight = 100f;  
+
+    private GameObject[] _boxes;
     // Start is called before the first frame update
-    void Start()
-    {   
+    private void Start()
+    {
+        _boxes = new GameObject[_tower.GetStackSize()];
         for (int i = 0; i < _tower.GetStackSize(); i++)
         {
             // Create a new box GameObject
-            GameObject box = Instantiate(boxPrefab, transform);
+            _boxes[i] = Instantiate(boxPrefab, transform);
+            _boxes[i].name = "TowerBox" + i;
 
             // Set the position and size of the box using RectTransform
-            RectTransform rectTransform = box.GetComponent<RectTransform>();
-            rectTransform.position = new Vector2(0f, -i);
-            rectTransform.sizeDelta = new Vector2(BoxWidth, BoxHeight);
+            RectTransform rectTransform = _boxes[i].GetComponent<RectTransform>();
+            rectTransform.localPosition = new Vector3(0, -i);
 
-            BoxPrefabScript boxPrefabScript = box.GetComponent<BoxPrefabScript>();
+            BoxPrefabScript boxPrefabScript = _boxes[i].GetComponent<BoxPrefabScript>();
             boxPrefabScript.SetColor(Constants.ColorForColor(_tower.GetLayerColor(i)));
         }
-        RectTransform towerRectTransform = GetComponent<RectTransform>();
-        towerRectTransform.sizeDelta = new Vector2(BoxWidth, BoxHeight*_tower.GetStackSize());
-        BoxCollider2D collider2D = GetComponent<BoxCollider2D>();
-        collider2D.size = new Vector2(2, _tower.GetStackSize());
-        collider2D.offset = new Vector2(0, -1.5f);
     }
 
     // Update is called once per frame
@@ -39,17 +33,17 @@ public class TowerRenderer : MonoBehaviour
         
     }
 
-    private void OnMouseUp()
-    {
-        _clicked = false;
-    }
-
     private void OnMouseOver(){
-        if(Input.GetMouseButtonDown(0) && !_clicked)
+        if(Input.GetMouseButtonDown(0))
         {
-            _isSelected = !_isSelected;
-            RectTransform rectTransform = GetComponent<RectTransform>();
-            rectTransform.position += (_isSelected ? -1 : 1) * new Vector3(0, 1, 0);
+            SetSelected(!_isSelected);
         }
     }
+
+    public void SetSelected(bool isSelected)
+    {
+        _isSelected = isSelected;
+        RectTransform rectTransform = GetComponent<RectTransform>();
+        rectTransform.localPosition += (_isSelected ? -1 : 1) * new Vector3(0, 1, 0);
+    } 
 }
